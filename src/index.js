dbe6165ddc9979734be10716e5b447e847fb636c
michael-k-oconnor <m.k.oconnor53@gmail.com>
2018-11-26 16:41:28 -0600

    Initial component integrations

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 // TODO: Render the `App` component to the DOM
 import App from './components/App.js';
+import Search from './components/Search.js';
 
+ReactDOM.render(<App/>, document.getElementById('app'));
+// ReactDOM.render(<Search />, document.getElementById('view'));
