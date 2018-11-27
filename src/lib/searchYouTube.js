f570dd32755e5290daf6e7a75681018dc00a9ed4
michael-k-oconnor <m.k.oconnor53@gmail.com>
2018-11-27 12:29:03 -0600

    Start Youtube API implementation

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,16 @@
+
+
 var searchYouTube = (options, callback) => {
-  // TODO
+  
+  $.get('https://www.googleapis.com/youtube/v3/search',
+    { 'maxResults': options.max,
+      'part' : 'snippet',
+      'q': options.query,
+      'videoEmbeddable': true,
+      'type': 'video',
+      'key': options.key }, callback(response)
+  );
+
 };
 
 export default searchYouTube;
